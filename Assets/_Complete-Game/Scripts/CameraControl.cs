6043c923e4e7b3724541ef3acab2dd1044a3f210
yayoitamura <gh3520900@gmail.com>
2017-10-13 19:29:35 +0900

    Cameraの追跡機能

---
+++ Assets/_Complete-Game/Scripts/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Completed
+{
+
+	public class CameraControl : MonoBehaviour
+	{
+
+		public GameObject player;
+        public int y = 5;
+        public int z = -10;
+
+		void Start()
+		{
+
+		}
+
+		void Update()
+		{
+			transform.position = new Vector3(player.transform.position.x, y, z);
+		}
+
+	}
+}

5fba599a53285e39fea13db9984c6014f92575ba
yayoitamura <gh3520900@gmail.com>
2017-11-01 16:00:47 +0900

    Item appear

--- Assets/_Complete-Game/Scripts/Wall.cs
+++ Assets/_Complete-Game/Scripts/Wall.cs
@@ -10,6 +10,7 @@ namespace Completed
         public AudioClip chopSound2;                //壁がPlayeに攻撃された時のオーディオクリッップ２
         public Sprite dmgSprite;                    //Awall破壊された時の差し換え用スプライト
         public int hp = 3;                          //wallのhp
+        public GameObject[] items;
 
         private SpriteRenderer spriteRenderer;      //Store a component reference to the attached SpriteRenderer.
                                                     //付属のSpriteRendererにコンポーネント参照を格納します。 ?
@@ -24,6 +25,7 @@ namespace Completed
         //Playerがwallを攻撃した時　Player.OnCantMove で呼ばれる
         public void DamageWall(int loss)
         {
+
             //SoundManagerのRandomizeSfx関数に二つのオーディオを渡す
             SoundManager.instance.RandomizeSfx(chopSound1, chopSound2);
 
@@ -34,8 +36,18 @@ namespace Completed
 
             //hpが0以下
             if (hp <= 0)
+            {
                 //SetActive(false)でgameObjectを無効(非アクティブ)
                 gameObject.SetActive(false);
+            }
+
+            //壁を壊すとアイテム
+            //壁が壊れるのとアイテム出現、取得が同時なので修正
+            if (!gameObject.activeSelf)
+            {
+                GameObject randumItem = items[Random.Range(0, items.Length)];
+                Instantiate(randumItem, transform.position, Quaternion.identity);
+            }
         }
     }
 }
